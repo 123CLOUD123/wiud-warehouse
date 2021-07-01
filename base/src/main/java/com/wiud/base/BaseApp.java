4bf68dd719ffb50ca60d22ff749418d86f0778b9
ZhangHao <hao.zhang@verymro.com>
2021-07-01 17:29:56 +0800

    fix

--- base/src/main/java/com/wiud/base/BaseApp.java
+++ base/src/main/java/com/wiud/base/BaseApp.java
@@ -2,12 +2,14 @@ package com.wiud.base;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 /**
  * Hello world!
  *
  */
 @SpringBootApplication
+//@EnableOAuth2Sso
 public class BaseApp {
     public static void main(String[] args) {
         SpringApplication.run(BaseApp.class, args);
