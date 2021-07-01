1aa00c2268669c327c923033b5cb6d6e1f8a3ad9
ZhangHao <102196124@qq.com>
2021-07-01 21:45:37 +0800

    初始化网关服务

---
+++ zuul-server/src/main/java/com/wiud/zuul/ZuulApp.java
@@ -0,0 +1,16 @@
+package com.wiud.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class ZuulApp {
+    public static void main( String[] args ) {
+    	SpringApplication.run(ZuulApp.class, args);
+    }
+}
