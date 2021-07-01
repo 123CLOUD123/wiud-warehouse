654b560e8db188b092bae98b3a2edae6b855c744
ZhangHao <hao.zhang@verymro.com>
2021-07-01 13:08:44 +0800

    初始化

---
+++ base/src/main/java/com/wiud/base/BaseApp.java
@@ -0,0 +1,15 @@
+package com.wiud.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class BaseApp {
+    public static void main(String[] args) {
+        SpringApplication.run(BaseApp.class, args);
+    }
+}
---
+++ base/src/main/java/com/wiud/base/rest/TestRest.java
@@ -0,0 +1,16 @@
+package com.wiud.base.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/base")
+public class TestRest {
+
+	@GetMapping("/test")
+	public String test() {
+		return "hello base";
+	}
+	
+}
