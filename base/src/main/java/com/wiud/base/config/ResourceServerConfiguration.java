7fc865cf9240970c1f3f237e3e36726974c6da5b
ZhangHao <hao.zhang@verymro.com>
2021-07-01 16:35:17 +0800

    add resource server config & maven dependencies

---
+++ base/src/main/java/com/wiud/base/config/ResourceServerConfiguration.java
@@ -0,0 +1,73 @@
+package com.wiud.base.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+
+@EnableResourceServer
+@Configuration
+public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+
+    @Autowired
+    private ResourceServerProperties resourceServerProperties;
+    
+//    @Autowired
+//    private GsUserAuthConverter gsUserAuthConverter;
+    
+    @Bean
+    public TokenStore tokenStore() {
+        return new InMemoryTokenStore();
+    }
+    
+    @Bean
+    public AccessTokenConverter accessTokenConverter() {
+        DefaultAccessTokenConverter accessTokenConverter = new DefaultAccessTokenConverter();
+//        accessTokenConverter.setUserTokenConverter(gsUserAuthConverter);
+        return accessTokenConverter;
+    }
+    
+    @Bean
+    public ResourceServerTokenServices tokenServices() {
+        RemoteTokenServices remoteTokenServices = new RemoteTokenServices();
+        remoteTokenServices.setCheckTokenEndpointUrl(resourceServerProperties.getTokenInfoUri());
+        remoteTokenServices.setClientId(resourceServerProperties.getClientId());
+        remoteTokenServices.setClientSecret(resourceServerProperties.getClientSecret());
+        remoteTokenServices.setAccessTokenConverter(accessTokenConverter());
+        return remoteTokenServices;
+    }
+    
+//    @Bean
+//    public UserInfoTokenServices userInfoTokenServices() {
+//        UserInfoTokenServices userInfoTokenServices = new UserInfoTokenServices(resourceServerProperties.getUserInfoUri(), resourceServerProperties.getClientId());
+//        return userInfoTokenServices;
+//    }
+    
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) {
+        resources.resourceId("base_app")
+            .tokenServices(tokenServices())
+            .stateless(true);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .antMatchers("/test/**").permitAll()
+            .antMatchers("/**").authenticated()
+            .and()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED);
+    }
+
+}
