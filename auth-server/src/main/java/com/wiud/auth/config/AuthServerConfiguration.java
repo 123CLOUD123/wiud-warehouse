ec56609705e9194f41f5c462fa183c74df07c1d1
ZhangHao <hao.zhang@verymro.com>
2021-07-01 16:22:40 +0800

    add security & authorization server configuration

--- auth-server/src/main/java/com/wiud/auth/config/AuthServerConfiguration.java
+++ auth-server/src/main/java/com/wiud/auth/config/AuthServerConfiguration.java
@@ -1,42 +1,67 @@
 package com.wiud.auth.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
-import org.springframework.security.oauth2.core.AuthorizationGrantType;
-import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
-import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
-import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
-import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 
 @Configuration
-@EnableWebSecurity
-@Import(OAuth2AuthorizationServerConfiguration.class)
-public class AuthServerConfiguration {
+@EnableAuthorizationServer
+public class AuthServerConfiguration extends AuthorizationServerConfigurerAdapter {
 
-	@Bean
-	public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().authenticated();
-		return http.build();
-	}
-	
-	@Bean
-    public RegisteredClientRepository registeredClientRepository() {
-        RegisteredClient client = RegisteredClient.withId("test_id")
-                .clientId("test_client")
-                .clientSecret("test_secret")
-                .clientAuthenticationMethod(ClientAuthenticationMethod.BASIC)
-                .authorizationGrantTypes(authorizationGrantTypes -> {
-                    authorizationGrantTypes.add(AuthorizationGrantType.AUTHORIZATION_CODE);
-                    authorizationGrantTypes.add(AuthorizationGrantType.REFRESH_TOKEN);
-                })
-                .redirectUri("https://baidu.com")
-                .build();
-        return new InMemoryRegisteredClientRepository(client);
+//    @Autowired
+//    private UserServiceImpl userService;
+
+//    @Autowired
+//    private ClientServiceImpl clientService;
+//
+//    @Autowired
+//    private GsUserAuthenticationConverter gsUserAuthenticationConverter;
+    
+//    @Autowired
+//    private GsAccessTokenConverter gsAccessTokenConverter;
+
+//    @Bean
+//    public NoOpPasswordEncoder passwordEncoder() {
+//        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+//    }
+
+    /**
+     * token 存储方式
+     */
+    @Bean
+    public TokenStore tokenStore() {
+        return new InMemoryTokenStore();
     }
-	
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.tokenStore(tokenStore())
+        		.allowedTokenEndpointRequestMethods(HttpMethod.GET, HttpMethod.POST);
+//        		.accessTokenConverter(gsAccessTokenConverter); // 密码模式需配置AuthenticationManager/UserDetailsService;
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.allowFormAuthenticationForClients() // 让/oauth/token 支持client_id以及client_secret作登录认证
+                .tokenKeyAccess("permitAll()")
+                .checkTokenAccess("permitAll()");
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+//        clients.withClientDetails(clientService);
+    	clients.inMemory()
+    		.withClient("client_id")
+    		.secret("client_secret");
+    }
+
 }
+
---
+++ auth-server/src/main/java/com/wiud/auth/config/WebSecurityConfiguration.java
@@ -0,0 +1,46 @@
+package com.wiud.auth.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+    	web.ignoring().antMatchers("/css/**");
+    	web.ignoring().antMatchers("/img/**");
+    }
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("test")
+			.password("test")
+			.roles("ADMIN");
+	}
+	
+	@Override
+    protected void configure(HttpSecurity http) throws Exception {
+		http.headers().frameOptions().disable()
+			.and().csrf().disable()
+			.cors().disable()
+			.httpBasic().disable()
+			.authorizeRequests()
+			.antMatchers("/test/**").permitAll()
+			.anyRequest().authenticated()
+			.and().formLogin()
+			.and().logout()
+				.logoutUrl("/oauth/logout")
+				.logoutSuccessUrl("/login")
+				.deleteCookies("JSESSIONID")
+				.invalidateHttpSession(true)
+				.clearAuthentication(true);
+    }
+
+}
